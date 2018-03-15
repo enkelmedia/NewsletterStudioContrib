1c8f94c1bdc044be5a474b7114c8b5686adca983
Markus Johansson <markus@obviuse.se>
2018-03-15 10:01:26 +0100

    Added events example

---
+++ Newsletter Studio V2/NewsletterStudioContrib/Events/EventsExample.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Owin.Security;
+using NewsletterStudio.Infrastucture.Events;
+using Umbraco.Core;
+
+namespace NewsletterStudioContrib.Events
+{
+    public class EventsExample : ApplicationEventHandler
+    {
+        protected override void ApplicationStarting(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+
+            NewsletterStudio.Services.SendNewsletterService.SendingUnique += SendNewsletterServiceOnSendingUnique;
+
+        }
+        private void SendNewsletterServiceOnSendingUnique(object sender, NewsletterSendingUniqueEventArgs newsletterSendingUniqueEventArgs)
+        {
+            newsletterSendingUniqueEventArgs.MailMessage.Headers.Add("yada","foo");
+            
+        }
+    }
+}
