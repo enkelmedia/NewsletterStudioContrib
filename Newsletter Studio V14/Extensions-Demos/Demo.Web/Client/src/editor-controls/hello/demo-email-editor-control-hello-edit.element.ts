80843f7d9efded3f2ee0776522f51584fb666911
Markus Johansson <markus@enkelmedia.se>
2024-11-24 14:54:22 +0100

    Worked on v14 demo

--- Newsletter Studio V14/Extensions-Demos/Demo.Web/Client/src/editor-controls/hello/demo-email-editor-control-hello-edit.element.ts
+++ Newsletter Studio V14/Extensions-Demos/Demo.Web/Client/src/editor-controls/hello/demo-email-editor-control-hello-edit.element.ts
@@ -1,10 +1,12 @@
 import { NsEmailEditorControlEditUiBase } from "@newsletterstudio/umbraco/extensibility";
 import { NsEmailEditorControlHelloData } from "./demo-email-editor-control-hello.models";
 import { css, customElement, html } from "@umbraco-cms/backoffice/external/lit";
+import { NsPaddingChangedEvent } from "@newsletterstudio/umbraco/components";
 
 @customElement('demo-email-editor-hello-edit')
 export class DemoEmailEditorHalloEditElement extends NsEmailEditorControlEditUiBase<NsEmailEditorControlHelloData> 
 {
+    
     #updateText(e : InputEvent){
         this.updateModel({
             text : (e.target as HTMLTextAreaElement).value 
@@ -18,12 +20,13 @@ export class DemoEmailEditorHalloEditElement extends NsEmailEditorControlEditUiB
             <div>
                 <ns-property label="Enter text">
                     <textarea .value=${this.model?.text ?? ''} @keyup=${this.#updateText}></textarea>                
-                </ns-property>                
+                </ns-property>
+
                 <ns-property label="ns_margin">
                     <ns-padding-editor
                         id="padding"
                         .value=${this.model!.padding}
-                        @change=${(e : any)=>this.updateModel({padding:e.detail})}></ns-padding-editor>
+                        @change=${(e : NsPaddingChangedEvent)=>this.updateModel({padding:e.detail})}></ns-padding-editor>
                 </ns-property>
             </div>
         `;
