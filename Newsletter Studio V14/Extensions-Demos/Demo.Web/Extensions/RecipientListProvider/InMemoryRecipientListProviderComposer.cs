2c1fb13e002f80ed0a0332b274358ae97c40bf56
Markus Johansson <markus@obviuse.se>
2024-12-12 23:06:56 +0100

    Make v14 demo site great again

--- Newsletter Studio V14/Extensions-Demos/Demo.Web/Extensions/RecipientListProvider/InMemoryRecipientListProviderComposer.cs
+++ Newsletter Studio V14/Extensions-Demos/Demo.Web/Extensions/RecipientListProvider/InMemoryRecipientListProviderComposer.cs
@@ -1,8 +1,9 @@
 ﻿using NewsletterStudio.Core.Composing;
+using Umbraco.Cms.Core.Composing;
 
 namespace Demo.Web.Extensions.RecipientListProvider;
 
-public class InMemoryRecipientListProviderComposer
+public class InMemoryRecipientListProviderComposer : IComposer
 {
     public void Compose(IUmbracoBuilder builder)
     {
