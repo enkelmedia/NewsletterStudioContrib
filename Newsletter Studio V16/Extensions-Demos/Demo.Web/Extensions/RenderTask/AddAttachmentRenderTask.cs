03b31d4a03a30a903e52bc2c66f62df4308d3192
Markus Johansson <markus@enkelmedia.se>
2025-06-13 01:41:05 +0200

    v16: added code samples for render task

---
+++ Newsletter Studio V16/Extensions-Demos/Demo.Web/Extensions/RenderTask/AddAttachmentRenderTask.cs
@@ -0,0 +1,20 @@
+﻿using NewsletterStudio.Core.Rendering;
+using NewsletterStudio.Core.Rendering.Tasks;
+using NewsletterStudio.Core.Sending;
+
+namespace Demo.Web.Extensions.RenderTask;
+
+public class AddAttachmentRenderTask : IRenderTask
+{
+    public void Process(RenderTaskProcessingResult result, RenderingUniqueContext context)
+    {
+        context.EmailMessage.Attachments.Add(GetAttachmentFor(context.Recipient));
+    }
+
+    private EmailAttachment GetAttachmentFor(IRecipientDataModel recipientDataModel)
+    {
+        using var stream = File.OpenRead("c:\\temp\\invoice.pdf");
+
+        return new EmailAttachment("Invoice123.pdf", stream);
+    }
+}
